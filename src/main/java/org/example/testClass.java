9a7c005aa91cb6ca19cdf8a76839be4268142fa9
sirajudeen <sirajuudeen@gmail.com>
2026-08-10 23:14:16 +0530

    changes main 2

--- src/main/java/org/example/testClass.java
+++ src/main/java/org/example/testClass.java
@@ -23,10 +23,10 @@ public class testClass {
         str.concat("world");
         System.out.println(str);
 
-        String str1="hello";
-        str1=str1.concat(" word");
-        System.out.println(str1);
-
+        String str5="hello";
+        str5=str5.concat(" word");
+        System.out.println(str5);
+        System.out.println("Chagnes applied 5");
 
 
     }
